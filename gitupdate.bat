b87ccc9b66852a84d44f3c1ba0ea99e50f7ad8a4
joabssilveira <joabssilveira@gmail.com>
2022-03-12 05:54:39 -0300

    default

---
+++ gitupdate.bat
@@ -0,0 +1,5 @@
+cd C:\Apache24\htdocs\unioticacolibri
+git add .
+git commit -m "default"
+git push origin main
+pause
---
+++ system/storage/cache/template/c3/c3cd62f6e6a8c134c453d061d76daf8546502a1689b81e908e382f5731766831.php
@@ -0,0 +1,105 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* default/template/common/home.twig */
+class __TwigTemplate_c2f4e2ded403e845ac49fa091f6f844fa6084d83857b7b898f3f5cf419c26ef1 extends \Twig\Template
+{
+    private $source;
+    private $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        // line 1
+        echo ($context["header"] ?? null);
+        echo "
+<div id=\"common-home\" class=\"container\">
+  <div class=\"row\">";
+        // line 3
+        echo ($context["column_left"] ?? null);
+        echo "
+    ";
+        // line 4
+        echo ($context["slideshow"] ?? null);
+        echo "
+    ";
+        // line 5
+        if ((($context["column_left"] ?? null) && ($context["column_right"] ?? null))) {
+            // line 6
+            echo "    ";
+            $context["class"] = "col-sm-6";
+            // line 7
+            echo "    ";
+        } elseif ((($context["column_left"] ?? null) || ($context["column_right"] ?? null))) {
+            // line 8
+            echo "    ";
+            $context["class"] = "col-sm-9";
+            // line 9
+            echo "    ";
+        } else {
+            // line 10
+            echo "    ";
+            $context["class"] = "col-sm-12";
+            // line 11
+            echo "    ";
+        }
+        // line 12
+        echo "    <div id=\"content\" class=\"";
+        echo ($context["class"] ?? null);
+        echo "\">";
+        echo ($context["content_top"] ?? null);
+        echo ($context["content_bottom"] ?? null);
+        echo "</div>
+    ";
+        // line 13
+        echo ($context["column_right"] ?? null);
+        echo "</div>
+</div>
+";
+        // line 15
+        echo ($context["footer"] ?? null);
+    }
+
+    public function getTemplateName()
+    {
+        return "default/template/common/home.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  83 => 15,  78 => 13,  70 => 12,  67 => 11,  64 => 10,  61 => 9,  58 => 8,  55 => 7,  52 => 6,  50 => 5,  46 => 4,  42 => 3,  37 => 1,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("", "default/template/common/home.twig", "");
+    }
+}
